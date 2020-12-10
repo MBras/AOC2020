8245bf84f9345b079555bb8b4cfb50a1c1aabfdd
Mark Braspenning <bras@sandcat.nl>
2020-12-10 14:21:37 +0100

    meer opruimen en nette output

--- 10/2.php
+++ 10/2.php
@@ -6,10 +6,6 @@ $lines = array_map('intval', $lines);
 sort($lines);
 array_unshift($lines, 0);
 
-print_r($lines);
-
-echo "Part 2: " . $counter;
-
 $sum = array(1);
 for ($x = 1; $x < sizeof($lines); $x++) {
     //check van alles wat voorafgaand in range is hoeveel stappen er naartoe gemaakt kunnen worden en tel die op
@@ -21,4 +17,5 @@ for ($x = 1; $x < sizeof($lines); $x++) {
     }
     $sum[] = $tempsum;
 }
-print_r($sum);
+echo "Part 2: " . array_pop($sum);
+//print_r($sum);
--- 10/index.php
+++ 10/index.php
@@ -1,15 +1,11 @@
 <?php
 
-$lines =  file("input.prod");
-
-$lines = array_map('intval', $lines);
+$lines = array_map('intval', file("input.prod"));
 sort($lines);
-//print_r($lines);
 
 $diffs = array(0,1,0,1);
 for ($x = 0; $x < sizeof($lines) - 1; $x++) {
     $diffs[$lines[$x + 1] - $lines[$x]]++;
 }
-//print_r($diffs);
 echo "Part 1: ". ($diffs[1] * $diffs[3]) . "<br>";
 
