4d0117ce003afbc2959266408cd8f30d8e040d0a
Mark Braspenning <bras@sandcat.nl>
2020-12-14 20:31:50 +0100

    cleanup and solution for part 2

--- 13/index.php
+++ 13/index.php
@@ -2,6 +2,7 @@
 
 $lines = file("input.1");
 $lines = file("input.2");
+//$lines[1] = "1789,37,47,1889"; //1202161486
 
 $start = intval($lines[0]);
 $busses = array_filter(explode(",", $lines[1]), function ($element) { return ($element != "x");}); 
@@ -17,3 +18,24 @@ foreach ($busses as $bus) {
     }
 }
 echo "Part 1: " . (($mindeparture - $start) * $minbus) ."<br>";
+
+//==============[ Part II ]=======================
+
+print_r($busses);
+echo "<br>";
+
+$stepsize = 1;
+$timestamp = 1;
+foreach ($busses as $buskey => $busval) {
+    if ($busval != "x") {
+        echo $buskey . " - " . $busval . "<br>";
+        while (1) {
+            if (($timestamp + $buskey) % $busval == 0) {
+                $stepsize *= $busval;
+                echo "Timestamp: " . $timestamp . ", step: " . $stepsize . "<br>";
+                break;
+            }
+            $timestamp += $stepsize;
+        }
+    }
+}
